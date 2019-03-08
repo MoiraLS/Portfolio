069b7cda53dbc394eb89b045796459f570c5b280
Moïra <maelle.lescouarnec@gmail.com>
2019-03-08 19:01:37 +0100

    Popup dans À propos totalement fonctionnelle (liens, bouton et position)

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,6 +3,12 @@ let popup = document.getElementById('popup');
 
 contact.addEventListener('click', function() {
   popup.classList.toggle('popup--visible');
+});
 
-  contact.innerHTML = 'Retour';
+contact.addEventListener('click', function() {
+  if (popup.className === 'popup--visible') {
+    contact.innerHTML = 'Retour';
+  } else {
+    contact.innerHTML = 'Me contacter';
+  };
 });
